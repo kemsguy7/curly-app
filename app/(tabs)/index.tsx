6ff541e5727b730296e4e10a857ea215ac13a79d
kemsguy7 <mattidungafa@gmail.com>
2026-04-05 10:48:22 +0100

    debug for fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,27 @@
 import '@/global.css';
 import { Link } from 'expo-router';
 
-import { Text } from 'react-native';
 import { styled } from 'nativewind';
+import { Text } from 'react-native';
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
 const SafeAreaView = styled(RNSafeAreaView);
 export default function Index() {
   return (
-    <SafeAreaView className='flex-1 items-center  bg-background p-5'>
-      <Text className='text-xl font-bold text-blue-500'>Welcome to Nativewind!.</Text>
-      <Link href='/onboarding' className='mt-4 rounded bg-primary text-white p-4'>
+    <SafeAreaView className='flex-1 items-center  bg-background pt-20'>
+      <Text className='text-7xl font-sans-extrabold'> Home </Text>
+      <Link href='/onboarding' className='mt-4 rounded  bg-primary font-sans-bold text-white p-4'>
         Go to Onboarding{' '}
       </Link>
-      <Link href='/(auth)/sign-in' className='mt-4 rounded bg-primary text-white p-4'>
+      <Link
+        href='/(auth)/sign-in'
+        className='mt-4 rounded bg-primary font-sans-bold text-white p-4'
+      >
         Go to Sign In{' '}
       </Link>
-      <Link href='/(auth)/sign-up' className='mt-4 rounded bg-primary text-white p-4'>
+      <Link
+        href='/(auth)/sign-up'
+        className='mt-4 rounded bg-primary font-sans-bold text-white p-4'
+      >
         {' '}
         Go to Sign Up{' '}
       </Link>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import '@/global.css';
-import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
 
 /** Without this, `(auth)` sorts before `(tabs)` (same “group index” tier, shorter name wins). */
 export const unstable_settings = {
@@ -7,5 +9,21 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
   return <Stack screenOptions={{ headerShown: false }} />;
 }
