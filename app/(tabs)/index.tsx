7a4c95e499b4319ec7046b2957084ae6adf06c49
kemsguy7 <mattidungafa@gmail.com>
2026-04-05 09:32:47 +0100

    fix: more tab fixes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import '@/global.css';
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
 
+import { Text } from 'react-native';
+import { styled } from 'nativewind';
+import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
+const SafeAreaView = styled(RNSafeAreaView);
 export default function Index() {
   return (
-    <View className='flex-1 items-center justify-center bg-background'>
+    <SafeAreaView className='flex-1 items-center  bg-background p-5'>
       <Text className='text-xl font-bold text-blue-500'>Welcome to Nativewind!.</Text>
       <Link href='/onboarding' className='mt-4 rounded bg-primary text-white p-4'>
         Go to Onboarding{' '}
@@ -25,6 +28,6 @@ export default function Index() {
       >
         Claude Max Subcription{' '}
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
